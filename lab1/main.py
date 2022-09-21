0a1ecff23a00115b1e3177a30de6fc614df9a3e0
markiankmet <markkmet13@gmail.com>
2022-09-21 22:47:53 +0300

    update file
    fix

--- lab1/main.py
+++ lab1/main.py
@@ -1,23 +1,33 @@
-def number_ways_to_cut_tree():   # function to count number of ways to cut trees
-    check_n = False
-    while not check_n:  # check if n is number
-        n = (input('Enter number of trees: '))
-        if n.isnumeric():
-            check_n = True
-            n = int(n)
+LIMITS1 = 1000
+LIMITS2 = 0
+
+
+def check_if_numeric(x):
+    check_x = False
+    while not check_x:  # check if x is number
+        if x.isnumeric():
+            check_x = True
+            x = int(x)
+            return x
         else:
-            print('Invalid Error! Try again!')
-    check_m = False
-    while not check_m:  # check if m is number
-        m = input('Enter number of trees that should remain: ')
-        if m.isnumeric():
-            check_m = True
-            m = int(m)
-        else:
-            print('Invalid Error! Try again!')
-    if n > 1000 or n < 0 or m > 1000 or m < 0:
-        print('Invalid value(0 > n,m > 1000)')
+            x = input('Invalid Error! Try again: ')
+
+
+def check_number_size(x):
+    if x > LIMITS1:
+        return False
+    elif x < LIMITS2:
+        return False
+    else:
+        return True
+
+
+def number_ways_to_cut_tree(n, m):   # function to count number of ways to cut trees
+    if not check_number_size(n):
+        print('Invalid value(n > 1000)')
         return
+    elif not check_number_size(m):
+        print('Invalid value(m > 1000)')
     elif m > n:  # check if m bigger than n
         print('Invalid value(m > n)')
         return
@@ -34,6 +44,28 @@ def number_ways_to_cut_tree():   # function to count number of ways to cut trees
         return res
 
 
-result = number_ways_to_cut_tree()
-print(f'Result = {result}.')
+menu_options = {1: 'Start program(Number of ways to cut trees)', 2: 'Exit'}
 
+
+def print_menu():
+    for key in menu_options.keys():
+        print(key, menu_options[key], sep='--')
+
+
+def main_program():
+    n = check_if_numeric(input('Enter number of trees: '))
+    m = check_if_numeric(input('Enter number of trees that should remain: '))
+    result = number_ways_to_cut_tree(n, m)
+    print(f'Result = {result}.')
+
+
+while True:
+    print_menu()
+    option = check_if_numeric(input('Enter your choice: '))
+    if option == 1:
+        main_program()
+    elif option == 2:
+        print('Good bye!')
+        exit()
+    else:
+        print('Invalid option. Please enter a number 1 or 2!')
